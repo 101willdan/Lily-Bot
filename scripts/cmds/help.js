7fe24da35ebffc36dafb0eabd1284bb55a4b90a7
Wìll <163330648+101willdan@users.noreply.github.com>
2024-06-23 12:25:06 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ Lily Bot ♥️ ]";
+const doNotDelete = "[💖Lily Bot💖]";
 /**
 * @author NTKhang
 * @author: do not delete it
